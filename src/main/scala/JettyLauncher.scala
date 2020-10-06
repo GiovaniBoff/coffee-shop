430bfa3a19a47087acb4d0879b93a799813892ed
githiago-f <tfarias@protonmail.com>
2020-10-05 23:16:03 -0300

    fix: changing from sys env to sys props

--- src/main/scala/JettyLauncher.scala
+++ src/main/scala/JettyLauncher.scala
@@ -3,17 +3,19 @@ import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.webapp.WebAppContext
 import org.scalatra.servlet.ScalatraListener
 
-object JettyLauncher extends App {
-  val port = sys.env.getOrElse("PORT", 8080).toString.toInt
-  val server = new Server(port)
-  val context = new WebAppContext()
+object JettyLauncher {
+  def main(args: Array[String]): Unit = {
+    val port = sys.props.getOrElse("http.port", "8080").toInt
+    val server = new Server(port)
+    val context = new WebAppContext
 
-  context setContextPath "/"
-  context setResourceBase "src/main/webapp"
-  context setEventListeners Array(new ScalatraListener)
-  context.setInitParameter(ScalatraListener.LifeCycleKey, "com.thiago_dev.ScalatraBootstrap")
-  server setHandler context
+    context.setContextPath("/")
+    context.setResourceBase("src/main/webapp")
+    context.setEventListeners(Array(new ScalatraListener))
+    context.setInitParameter(ScalatraListener.LifeCycleKey, "com.thiago_dev.ScalatraBootstrap")
+    server.setHandler(context)
 
-  server.start()
-  server.join()
+    server.start()
+    server.join()
+  }
 }
