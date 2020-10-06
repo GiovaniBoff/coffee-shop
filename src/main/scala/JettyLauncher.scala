4ec19775b5a5483a0db4646dfe86cd7d0c59fda9
githiago-f <tfarias@protonmail.com>
2020-10-05 23:48:33 -0300

    fix: removing custom lifecycle - keep all the same at that point.

--- src/main/scala/JettyLauncher.scala
+++ src/main/scala/JettyLauncher.scala
@@ -1,18 +1,20 @@
-
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.webapp.WebAppContext
 import org.scalatra.servlet.ScalatraListener
 
 object JettyLauncher {
+  println("Running!")
+
   def main(args: Array[String]): Unit = {
     val port = sys.props.getOrElse("http.port", "8080").toInt
+    println(port)
+
     val server = new Server(port)
     val context = new WebAppContext
 
     context.setContextPath("/")
     context.setResourceBase("src/main/webapp")
     context.setEventListeners(Array(new ScalatraListener))
-    context.setInitParameter(ScalatraListener.LifeCycleKey, "com.thiago_dev.ScalatraBootstrap")
     server.setHandler(context)
 
     server.start()
